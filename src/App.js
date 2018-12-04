fc8a98a8be88051b24a8cd69839cf7d39eb968a1
Michelle Yenny <mmy491@gmail.com>
2018-12-04 16:00:33 -0500

    added data to state and diplayed values

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import axios from 'axios'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      joke: '',
+      punchline: ''
+    }
+  }
   getRandomJoke = event => {
     axios
       .get('https://safe-falls-22549.herokuapp.com/random_joke')
       .then(response => {
         console.log(response.data)
+        const newState = {
+          joke: response.data.setup,
+          punchline: response.data.punchline
+        }
+        this.setState(newState)
       })
+    console.log('hello')
   }
 
   render() {
     return (
       <div className="App">
         <h1>Random Jokes Generator</h1>
-        <p />
+        <p>{this.state.joke}</p>
+        <p>{this.state.punchline}</p>
         <button onClick={this.getRandomJoke}>Click Here</button>
       </div>
     )
